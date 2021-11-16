3b82c20538b832bb49510d7810061689cbcb4bbe
SajidTechnicalHub <sajid.mayoo@gmail.com>
2021-11-16 19:37:22 +0200

    remoze amazone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.serve_static_assets = true
   
-    config.active_storage.service = :amazon
   # Code is not reloaded between requests.
   config.cache_classes = true
 
